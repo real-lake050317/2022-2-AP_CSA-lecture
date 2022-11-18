f5772da25eb0424e4882902dcb7a946c506bb6f6
Jinho Kim <79720217+real-lake050317@users.noreply.github.com>
2022-11-18 13:50:51 +0900

    .

--- Codes/ch8/Image.java
+++ Codes/ch8/Image.java
@@ -1,18 +1,42 @@
 public class Image {
     private final int BLACK = 1;
     private final int WHITE = 0;
-    private int[][] image;
+    private int[][][] image; // 3D array of pixels, row, col, info (colour, visit sequence, color change sequence)
     private int size;
+    private int visitSequence = 0;
+    private int colorChangeSequence = 0;
 
     public Image() {
         this.size = 10;
-        this.image = new int[this.size][this.size];
+        this.image = new int[this.size][this.size][3];
     }
 
-    public void display() {
+    public Image(int size) {
+        this.size = size;
+        this.image = new int[this.size][this.size][3]; 
+    }
+
+    public Image(int size, int[][] board) {
+        this.size = size;
+        this.image = board;
+    }
+
+    public void randomize(double factor) {
         for (int i = 0; i < this.size; i++) {
             for (int j = 0; j < this.size; j++) {
-                if (this.image[i][j] == BLACK) {
+                if (Math.random() < factor) {
+                    this.image[i][j][0] = BLACK;
+                } else {
+                    this.image[i][j][0] = WHITE;
+                }
+            }
+        }
+    }
+
+    public void displayImage() {
+        for (int i = 0; i < this.size; i++) {
+            for (int j = 0; j < this.size; j++) {
+                if (this.image[i][j][0] == BLACK) {
                     System.out.print("*");
                 } else {
                     System.out.print("0");
@@ -22,10 +46,32 @@ public class Image {
         }
     }
 
+    public void displayInfo() {
+        System.out.println("Visit Sequence");
+        for (int i = 0; i < this.size; i++) {
+            for (int j = 0; j < this.size; j++) {
+                System.out.print(this.image[i][j][1] + "  ");
+            }
+            System.out.println();
+        }
+        System.out.println("Color Change Sequence");
+        for (int i = 0; i < this.size; i++) {
+            for (int j = 0; j < this.size; j++) {
+                System.out.print(this.image[i][j][2] + "  ");
+            }
+            System.out.println();
+        }
+    }
+
     public void eraseBlob(int row, int col) {
         if (row >= 0 && row < size && col >= 0 && col < size) {
-            if (image[row][col] == BLACK) {
-                image[row][col] = WHITE;
+            if (image[row][col][1] == 0 || visitSequence == 0) {
+                image[row][col][1] = ++visitSequence;
+            }
+
+            if (image[row][col][0] == BLACK) {
+                image[row][col][0] = WHITE;
+                image[row][col][2] = colorChangeSequence++;
                 eraseBlob(row - 1, col);
                 eraseBlob(row + 1, col);
                 eraseBlob(row, col - 1);
---
+++ Codes/ch8/ImageTest.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class ImageTest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the image: ");
+        int size = sc.nextInt();
+        System.out.print("Enter the density of the image: ");
+        double density = sc.nextDouble();
+        Image image = new Image(size);
+        image.randomize(density);
+        System.out.println("Randomized Image");
+        image.displayImage();
+        System.out.println();
+        System.out.print("Enter the row number of the blob to erase: ");
+        System.out.print("Enter the column number of the blob to erase: ");
+        int row = sc.nextInt();
+        int col = sc.nextInt();
+        image.eraseBlob(row, col);
+        System.out.println("Erased Blob at (" + row + ", " + col + ")");
+        image.displayImage();
+        System.out.println("\n");
+        image.displayInfo();
+    }
+}
